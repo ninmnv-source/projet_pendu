bf6955bc2a34dbecd1eab72260db53ceda9def9c
Nino <Nin.mnv@gmail.com>
2026-05-16 11:36:24 -0400

    fonction jouer partie

--- pendu.py
+++ pendu.py
@@ -1,3 +1,4 @@
+import random 
 
 def message_accueil():
     print("=" * 50)
@@ -22,7 +23,7 @@ Règles du jeu :
   """)
 
 
-#%%
+
 def charger_mot():
 # on demande ce que l'utilisateur préfère entre le fchier par defaut et son fichier 
 #lower() transforme majuscules en minuscules, pas de risque d'avoir de problèmes de majuscules avec la prmière lettre
@@ -48,10 +49,75 @@ def charger_mot():
         return mots
 
 
-  #%%
-  # test rapide
-  mots = charger_mot()
-  print(f"{len(mots)} mots chargés")
-  print(mots[:5])
+
+# test rapide
+mots = charger_mot() #quand seule cette ligne est active, ca se lance, quand aucune ligne de test est active ca se lance pas)
+#print(f"{len(mots)} mots chargés")
+#print(mots[:5])
+
+def choisir_mot(mots):
+    return random.choice(mots) 
+
+#print(choisir_mot(mots))
 
 
+def afficher_etat(mot, lettre_trouvees):
+    affichage = []
+    for lettre in mot :
+        if lettre in lettre_trouvees:
+            affichage.append(lettre) 
+        else :
+            affichage.append("_")
+    print(" ".join(affichage))      #join colle tous les elements de la listre affichage par un espace 
+
+
+
+#afficher_etat("carbone", set())
+#afficher_etat("carbone", {"c"})
+#afficher_etat("carbone", {"c", "o", "e"})
+
+
+
+def jouer_partie(mot):
+    chance = 6
+    lettres_trouvees = set()
+    lettres_jouees = set()
+
+#le temps qu'on a encore une chance et qu'on a pas trouvé le mot on continue la boucle 
+    while chance >0 and set(mot) != lettres_trouvees :
+        afficher_etat(mot, lettres_trouvees)  #on apelle la fonction qui affiche l'état du mot
+
+        mauvaises = lettres_jouees - set(mot)
+
+        if mauvaises:                   #si mauvaises lettres existent deja et pas deja dans le mot
+            print(f"Lettres ratées : {', '.join(sorted(mauvaises))}")
+            #monter les lettres deja jouées affiché dans un ordre alphabet
+
+        if chance >1 : 
+            print(f'Chances restantes : {chance}')
+        if chance == 1 :
+            print(f'Chance restante : {chance}')
+
+        lettre = input("Propose une lettre : ").lower()         #on prend la lettre de l'utilisateur
+
+#si lettre deja jouée on lui dit de donner une auttre lettre 
+        if lettre in lettres_jouees:
+            print("Déjà essayée ! Change de lettre! ")
+            continue                #sauter au tour suivant sans rien faire (redemander une lettre sans enelever de chance)
+        lettres_jouees.add(lettre)
+
+#si la lettre est dans le mot, le montrer, sinon lui dire qu'elle n'y ets pas 
+        if lettre in mot : 
+            lettres_trouvees.add(lettre)
+
+        else : 
+            chance -=1
+            print("mauvaise lettre ! dommaaaage..")
+        
+    if chance > 0:
+        print(f'Gagné!  Le mot étai : {mot}')
+    else : 
+        print(f'Perdu! Le mot était : {mot}')
+
+
+jouer_partie("chat")
